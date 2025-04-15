8069835f46aa7d53bd78bd66b044bee1fc832b76
Pedro Henrique Satoru <143213097+PedroSatoru@users.noreply.github.com>
2025-04-15 18:19:14 +0000

    aplicação .env

--- Codigos/testeConsistencia.py
+++ Codigos/testeConsistencia.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 from supabase import create_client, Client
 import pandas as pd
+import os
 
 # Configuração do Supabase
 SUPABASE_URL = os.getenv("SUPABASE_URL")
